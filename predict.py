ffc41aa96da618cf5b0618046ccfa159c12769a5
lishaozong <lishaozong@hikvision.com.cn>
2021-08-12 15:40:39 +0800

    预测过程
    预测过程no_grad

--- predict.py
+++ predict.py
@@ -8,14 +8,16 @@ import torch
 from lstm.tokenizer import LstmTokenzier
 
 if __name__=="__main__":
-    text="我高兴啊"
+    text="蛋糕真好吃啊"
     tokenizer =LstmTokenzier()
     input_ids = tokenizer.token(text=text,max_length=64)
     input_ids = torch.tensor(input_ids,dtype=torch.long).view(1,-1)
     #GPU下训练的模型，在CPU下运行需要注意：
     #模型加载方式：torch.load(模型文件,map_location="cpu").module
     model = torch.load("./model/lstm.pt",map_location="cpu").module
-    output,loss = model(input_ids)
+    with torch.no_grad():
+        output,loss = model(input_ids)
+    print(f"the output is {output}")
     index = torch.argmax(output,dim=1)
     label = tokenizer.get_label_name(index)
     print(label)
